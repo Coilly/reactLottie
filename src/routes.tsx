17be86dc50062185db41e4325c58ffbb7862995a
Coilly <37950570+Coilly@users.noreply.github.com>
2025-03-07 19:10:10 +0900

    feat: 기본세팅 추가

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,6 @@ import { lazy } from "react";
 
 const Home = lazy(() => import("./pages/Home"));
 
-const routes = [{ path: "/app", element: <Home /> }];
+const routes = [{ path: "/", element: <Home /> }];
 
 export default routes;
